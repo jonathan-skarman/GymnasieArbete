a2946b9eb5a43baa9ad427e1705baac135caabae
Victor von Corswant <victorcorswant@gmail.com>
2024-08-27 20:36:30 +0200

    Working pack and unpack

--- Code/test.rb
+++ Code/test.rb
@@ -10,7 +10,31 @@ hex = bin.unpack("H*").first
 puts hex
 #reads image file and prints in terminal as hex
 
+i = 0
+hex_array = []
+hex_array[0] = hex
+p hex_array
+
+puts "Converting back now"
+
+p back_bin = hex_array.pack('H*')
+p back_bin
+
 bamse = File.open('writing.bmp', 'w')
 #encode before writing to file
-bamse.puts(hex)
+bamse.puts(back_bin)
 bamse.close
+
+
+#Unused code for now
+
+=begin
+while i < hex.length
+  temp_sring = ""
+  temp_sring << hex[i]
+  temp_sring << hex[i + 1]
+  hex_array << temp_sring
+  i += 2
+end
+p hex_array
+=end
