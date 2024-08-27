55f2445c48151df38f190e4843f4643f3131a67d
jonathan skarman <kaboomgf@gmail.com>
2024-08-27 10:46:46 +0200

    added todo comment

--- Code/test.rb
+++ Code/test.rb
@@ -5,11 +5,12 @@
 #"4218414243".each_byte {|x| puts x.to_s(24)}
 
 
-bin = File.read("code/test3.bmp")
+bin = File.read("test3.bmp")
 hex = bin.unpack("H*").first
 puts hex
 #reads image file and prints in terminal as hex
 
 bamse = File.open('writing.bmp', 'w')
+#encode before writing to file
 bamse.puts(hex)
 bamse.close
