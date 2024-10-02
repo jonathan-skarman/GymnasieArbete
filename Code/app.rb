92ca0a20931db87600979c08927d3e73ae1a149e
Victor von Corswant <victorcorswant@gmail.com>
2024-10-02 09:53:19 +0200

    working writable version

---
+++ Code/app.rb
@@ -0,0 +1,91 @@
+#Str to bin
+def str_to_bin(str)
+	arr = str.bytes
+	arr.each_index {|index| arr[index] = "0" + (arr[index].to_i).to_s(2)}
+	str = ""
+	arr.each {|el| str += el}
+	str
+end
+
+#Hex to bin
+#"\xff\x00".unpack("b*") # => ["1111111100000000"]
+def hex_to_bin(hex_str)
+	bin_arr_str = hex_str.unpack("b*") #gives an array with only one string
+	bin_arr = []
+	i = 0
+	k = 0
+	while i < bin_arr_str[0].length
+		j = 0
+		temp = ""
+		while j < 8
+			temp += bin_arr_str[0][i]
+			j += 1
+			i += 1
+		end
+		bin_arr << temp
+		k += 1
+	end
+	return bin_arr
+end
+
+#Bin to hex
+#['11111111' + '00000000'].pack('b*') # => "\xFF\x00"
+def bin_to_hex(bin_arr)
+	bin_str = [""]
+	bin_arr.each {|bin_number| bin_str[0] += bin_number}
+	hex_str = bin_str.pack("b*")
+end
+
+def hex_to_img_formater(hex_str_input)
+	hex_str_output = ""
+
+
+	hex_str_output = hex_str_input
+end
+
+#Hex to img
+def hex_to_img(hex_str, search_path)
+	p hex_str
+
+
+
+	write = File.open(search_path, 'w')
+	write.puts(hex_str)
+	write.close
+end
+
+#Img to hex
+def img_to_hex(search_path)
+	read = File.read(search_path)
+	p read
+end
+
+#str_bin is the string to be hidden in the image
+#img_bin_arr is the binare data of the image, with each bit in an array
+#aggression is an integer between 1 and 8, that decides how many bits to change in each byte
+def hider(str_bin, img_bin_arr, aggression)
+	#kolla om sträng får plats här!
+
+	#i = 0
+	#img_bin_arr.each {|byte| byte[7] = str_bin[i]; i+=1}
+
+
+	#str_bin = "1010"
+	i = 40
+	j = 0
+	while i < img_bin_arr.length && str_bin[j] != nil
+		img_bin_arr[i][0] = str_bin[j]
+		i += 1
+		j += 1
+	end
+	img_bin_arr
+end
+
+def main(text_str, input_search_path, aggression, output_search_path)
+	hex_to_img(hex_to_img_formater(bin_to_hex(hider(str_to_bin(text_str), hex_to_bin(img_to_hex(input_search_path)), aggression))), output_search_path)
+	p "Exited successfully"
+end
+
+#puts "hej"
+
+main("", "smilebig.bmp", 8, "hider.bmp")#Agg(1-8)
--- Code/j-test.rb
+++ Code/j-test.rb
@@ -1,22 +1,72 @@
+#Str to bin
 def str_to_bin(str)
-  arr = str.bytes
-  arr.each_index {|index| arr[index] = arr[index].to_s(2)}
-  arr
+	arr = str.bytes
+	arr.each_index {|index| arr[index] = "0" + (arr[index].to_i).to_s(2)}
+	arr
 end
 
-def bin_to_str(arr)
-  arr2 = []
-  arr.each {|el| arr2 << el.to_i}
-  #arr2.pack('c*')
+#Hex to bin
+#"\xff\x00".unpack("b*") # => ["1111111100000000"]
+def hex_to_bin(hex_str)
+	bin_arr_str = hex_str.unpack("b*") #gives an array with only one string
+	bin_arr = []
+	i = 0
+	k = 0
+	while i < bin_arr_str[0].length
+		j = 0
+		temp = ""
+		while j < 8
+			temp += bin_arr_str[0][i]
+			j += 1
+			i += 1
+		end
+		bin_arr << temp
+		k += 1
+	end
+	return bin_arr
 end
 
-#p str_to_bin("Hello World")
-#p bin_to_str(["1001000", "1100101", "1101100", "1101100", "1101111", "100000", "1010111", "1101111", "1110010", "1101100", "1100100"])
-#p 2**8
+#Bin to hex
+#['11111111' + '00000000'].pack('b*') # => "\xFF\x00"
+def bin_to_hex(bin_arr)
+	bin_str = [""]
+puts "bamse"
+	p bin_str
+	p bin_arr
+	bin_arr.each {|bin_number| bin_str += bin_number; p bin_str; p bin_number}
+	hex_str = bin_str.pack("b*")
+end
 
-#p "hello world".unpack()
-#p "hello world".bytes.pack('c*')
-#p "H".bytes
-p [72].pack('c*')
+#Hex to img
+def hex_to_img(hex_str, search_path)
+	write = File.open(search_path, 'w')
+	write.puts(hex_str)
+	write.close
+end
 
-# .pack('c*')    decimal to str
+#Img to hex
+def img_to_hex(search_path)
+	read = File.read(search_path)
+end
+
+#str_bin is the string to be hidden in the image
+#img_bin_arr is the binare data of the image, with each bit in an array
+#aggression is an integer between 1 and 8, that decides how many bits to change in each byte
+def hider(str_bin, img_bin_arr, aggression)
+	i = 0
+
+	img_bin_arr.each do |byte|
+ 		byte[7] = str_bin[i]
+ 		i += 1
+ 		p i
+	end
+end
+
+def main(text_str, input_search_path, aggression, output_search_path)
+	hex_to_img(bin_to_hex(hider(str_to_bin(text_str), hex_to_bin(img_to_hex(input_search_path)), aggression)), output_search_path)
+	p "test"
+end
+
+#puts "hej"
+
+main("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "test3.bmp", 8, "hider.bmp")#Agg(1-8)
